7778b648dcc9a792ea3e949f21c9d33359043930
Wik-i <849541805@qq.com>
2020-10-31 14:51:07 +0800

    update controller
    更新控制器代码，连接mongodb数据库并封装增删改查等操作。

--- controller/constants.py
+++ controller/constants.py
@@ -1,1 +1,14 @@
-#add constants here
+MONGO_URI = "mongodb://localhost:27017"
+DATABASE = "LinkedOut"
+
+# POST_COLLECTION = "posts"
+# COMMENT_COLLECTION = "comments"
+
+
+COMMENT_COLLECTION = "comments"
+BLOG_COLLECTION = "blogs"
+
+STUDENT_COLLECTION = "students"
+COMMUTER_COLLECTION = "commuter"
+
+
--- controller/database.py
+++ controller/database.py
@@ -1,1 +1,35 @@
-#add database file here
+import pymongo
+from . import constants
+
+class Database(object):
+    URI = constants.MONGO_URI
+    DATABASE = None
+
+    @staticmethod
+    def initialize():
+        client = pymongo.MongoClient(Database.URI)
+        Database.DATABASE = client[constants.DATABASE]
+
+    @staticmethod
+    def insert(collection, data):
+        Database.DATABASE[collection].insert(data)
+
+    @staticmethod
+    def find(collection, query):
+        return Database.DATABASE[collection].find(query)
+
+    @staticmethod
+    def find_one(collection, query):
+        return Database.DATABASE[collection].find_one(query)
+
+    @staticmethod
+    def update(collection, old_values, new_values):
+        return Database.DATABASE[collection].update_one(old_values, new_values)
+
+    @staticmethod
+    def delete(collection, query):
+        Database.DATABASE[collection].delete_one(query)
+
+    @staticmethod
+    def deletemany(collection, query):
+        Database.DATABASE[collection].delete_one(query)
